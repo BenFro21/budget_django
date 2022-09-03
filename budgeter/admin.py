e97817ae35a8712849d0bae22fba80698b2b73eb
benfro21 <broad0601@gmail.com>
2022-09-03 13:17:13 -0400

    BB-Rough_draft_models

--- budgeter/admin.py
+++ budgeter/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Budget, Expenses
 
 # Register your models here.
+admin.site.register(Budget)
+admin.site.register(Expenses)
--- budgeter/models.py
+++ budgeter/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Budget(models.Model):
+    title = models.CharField(max_length=100, default='no title')
+    # date_created = models.DateField(("Date"), default=datetime.date)
+    budget_for = models.CharField(max_length=100, default='Misc.')
+    income = models.IntegerField(default=0, blank=True, editable=True)
+    total = models.IntegerField(blank=True, editable=True)
+    
+    def __str__(self):
+        return self.title
+    
+    
+class Expenses(models.Model):
+    type_choices = (
+       ('Misc', 'Miscellaneous'),
+       ('Car', 'Car'),
+       ('Elec', 'Electric'),
+       ('Util', 'Utilites'),
+       ('Food','Grocery'),
+       ('Fun', 'Entertainment'),
+       ('Kids', 'Kids'),
+       ('Pets', 'Pets') 
+    )
+    budget = models.ForeignKey(Budget, on_delete=models.CASCADE, related_name='expenses')
+    title = models.CharField(max_length=100)
+    # date_created = models.DateField(("Date"), default=datetime.date)
+    biller = models.CharField(max_length=100)
+    amount_planned = models.IntegerField(default=0, blank=True, editable=True)
+    amount_actual = models.IntegerField(default=0, blank=True, editable=True)
+    type_bill = models.CharField(max_length=4, choices=type_choices)
+    
+    def __str__(self):
+       return f"{self.title} part of {self.budget} budget" 
+       
