4b723e20103bd9aa4b2623be06f0865b4ebb5749
benfro21 <broad0601@gmail.com>
2022-09-10 17:33:38 -0400

    made migrations and Forign key to user model

--- budgeter/models.py
+++ budgeter/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
 
 class Budget(models.Model):
     type_choices = (
@@ -14,7 +15,7 @@ class Budget(models.Model):
     budget_for = models.CharField(max_length=100, default='Misc.')
     income = models.IntegerField(default=0, blank=True, editable=True)
     total = models.IntegerField(blank=True, editable=True)
-    
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, on_delete=models.CASCADE, related_name='created_by')
     def __str__(self):
         return self.title
     
