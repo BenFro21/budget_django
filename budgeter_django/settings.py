abb0aef066426e2cadcdfc56d0b09ae9f6a3b23f
benfro21 <broad0601@gmail.com>
2022-09-19 19:33:12 -0400

    l

--- budgeter_django/settings.py
+++ budgeter_django/settings.py
@@ -150,8 +150,8 @@ REST_FRAMEWORK = {
     ],
 }
 ALLOWED_HOSTS = ['*']
-
-CORS_ALLOWED_ORIGINS = ["https://budgetapp21.netlify.app",] # Use whichever port your React Frontend is running on
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = ["https://budgetapp21.netlify.app",] # Use whichever port your React Frontend is running on
 # This option will also need to be configured to include your Applications URL when deployed online      
 CORS_ALLOW_METHODS = [
     "DELETE",
